bdbb9717ab7a8a6f649e6da3db73bfb91a9205c7
daisvke <daisuketanigawa@live.fr>
2023-07-06 01:07:29 +0200

    ASM XOR cipher works in the C program

--- srcs/process_data.c
+++ srcs/process_data.c
@@ -3,7 +3,8 @@
 void	_ww_process_mapped_data()
 {
     char*	_key = _ww_keygen(_WW_KEYCHARSET, _WW_KEYSTRENGTH);
-	
     printf("Generated key: %s\n", _key);
+    xor_encrypt_decrypt(_key, _WW_KEYSTRENGTH, _buffer, _file_size);
+    xor_encrypt_decrypt(_key, _WW_KEYSTRENGTH, _buffer, _file_size);
     free(_key);
 }
