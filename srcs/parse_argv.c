0a235b6f94e0a0c074d06952ae5df9f81919bb4a
daisvke <daisuketanigawa@live.fr>
2023-07-10 16:02:10 +0200

    Modify macro name + minor correction

--- srcs/parse_argv.c
+++ srcs/parse_argv.c
@@ -1,22 +1,22 @@
 #include "ww.h"
 
-int	_ww_parse_argv(char *argv[])
+int _ww_parse_argv(char *argv[])
 {
 	// For testing
 	// Region from the source file to encrypt
-	_modes |= _WW_CYPTREG_PHDR,				// Select region using segments
-	_modes |= _WW_CYPTREG_PHTEXTX;			// .text segments with x rights
+	_modes |= _WW_CYPTREG_PHDR,			// Select region using segments
+	_modes |= _WW_CYPTREG_PHTEXTX;		// .text segments with x rights
 	// _modes |= _WW_CYPTREG_PHTEXT,		// .text segments
 	// _modes |= _WW_CYPTREG_PHALL,			// All segments
-	_modes |= _WW_CYPTREG_SHDR,				// Select region using sections
+	_modes |= _WW_CYPTREG_SHDR, // Select region using sections
 	// _modes |= _WW_CYPTREG_SHTEXT,	 	// text section
 	// _modes |= _WW_CYPTREG_SHDATA,	 	// data section
 	// _modes |= _WW_CYPTREG_SHALL			// text + data sections
 
 	// For testing
 	// Unpacker and stub insertion region in the target file
-	_modes |= _WW_INSTREG_PAD;				// Insert inside segments paddings
-	// _modes |= _WW_INSTREG_SFT,			// Insert at 0x0 and shift the rest
-	// _modes |= _WW_INSTREG_END			// Insert at the end of the file
+	_modes |= _WW_INJTREG_PAD; // Insert inside segments paddings
+	// _modes |= _WW_INJTREG_SFT,			// Insert at 0x0 and shift the rest
+	// _modes |= _WW_INJTREG_END			// Insert at the end of the file
 	return 0;
 }
--- ww.h
+++ ww.h
@@ -1,84 +1,84 @@
 #ifndef WOODY_WOODPACKER_H
-# define WOODY_WOODPACKER_H
+#define WOODY_WOODPACKER_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdint.h>
-# include <sys/mman.h>		// For mapping 
-# include <elf.h>			// For ELF header
-# include <string.h>
-# include <time.h>			// For rand & srand
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/mman.h> // For mapping
+#include <elf.h>	  // For ELF header
+#include <string.h>
+#include <time.h> // For rand & srand
 
-# include "errors.h"
+#include "errors.h"
 
 /*-------------------------------------------------------*/
 
 /*  Colors */
-# define _WW_RED_COLOR		"\033[31m"
-# define _WW_GREEN_COLOR	"\033[32m"
-# define _WW_YELLOW_COLOR	"\033[33m"
-# define _WW_RESET_COLOR	"\033[0m"
+#define _WW_RED_COLOR "\033[31m"
+#define _WW_GREEN_COLOR "\033[32m"
+#define _WW_YELLOW_COLOR "\033[33m"
+#define _WW_RESET_COLOR "\033[0m"
 
 /* Return */
-# define _WW_ERROR		1
+#define _WW_ERROR 1
 
 /* String header for output file
 	This header is an indication that the file has
 	been encrypted by this program
 */
-# define _WW_PROG_HEADER	"....WOODY....\n"
+#define _WW_PROG_HEADER "....WOODY....\n"
 
 /* Encryption */
 // Charset used for the encryption key
-# define _WW_KEYCHARSET		"abcdefghijklmnopqrstuvwxyz" \
-							"ABCDEFGHIJKLMNOPQRSTUVWXYZ" \
-							"0123456789"
+#define _WW_KEYCHARSET "abcdefghijklmnopqrstuvwxyz" \
+					   "ABCDEFGHIJKLMNOPQRSTUVWXYZ" \
+					   "0123456789"
 // Width of the key
-# define _WW_KEYSTRENGTH	18
+#define _WW_KEYSTRENGTH 18
 
 // Region from the source file to encrypt
 enum _ww_e_crypt_region
 {
-	_WW_CYPTREG_PHDR = 1,		// Select region using segments
-	_WW_CYPTREG_PHTEXTX = 2,	// .text segments with x rights
-	_WW_CYPTREG_PHTEXT = 4,		// .text segments
-	_WW_CYPTREG_PHALL = 8,		// All segments
-	_WW_CYPTREG_SHDR = 16,		// Select region using sections
-	_WW_CYPTREG_SHTEXT = 32,	// text section
-	_WW_CYPTREG_SHDATA = 64,	// data section
-	_WW_CYPTREG_SHALL = 128		// text + data sections
+	_WW_CYPTREG_PHDR = 1,	 // Select region using segments
+	_WW_CYPTREG_PHTEXTX = 2, // .text segments with x rights
+	_WW_CYPTREG_PHTEXT = 4,	 // .text segments
+	_WW_CYPTREG_PHALL = 8,	 // All segments
+	_WW_CYPTREG_SHDR = 16,	 // Select region using sections
+	_WW_CYPTREG_SHTEXT = 32, // text section
+	_WW_CYPTREG_SHDATA = 64, // data section
+	_WW_CYPTREG_SHALL = 128	 // text + data sections
 
 	// Unpacker and stub insertion region in the target file
 };
 
-// Unpacker and stub insertion region in the target file
-enum _ww_e_insert_region
+// Unpacker and stub injection region in the target file
+enum _ww_e_inject_region
 {
-	_WW_INSTREG_PAD = 256,		// Insert inside segments paddings
-	_WW_INSTREG_SFT = 512,		// Insert at 0x0 and shift the rest
-	_WW_INSTREG_END = 1024		// Insert at the end of the file
+	_WW_INJTREG_PAD = 256, // Insert inside segments paddings
+	_WW_INJTREG_SFT = 512, // Insert at 0x0 and shift the rest
+	_WW_INJTREG_END = 1024 // Insert at the end of the file
 };
 
 /*-------------------------------------------------------*/
 // Global variables
-extern unsigned char*	_mapped_data;
-extern off_t	        _file_size;
-extern uint16_t			_modes;
+extern unsigned char *_mapped_data;
+extern off_t _file_size;
+extern uint16_t _modes;
 /*-------------------------------------------------------*/
 
-size_t	_ww_strlen(const char *s);
+size_t _ww_strlen(const char *s);
 
-int		_ww_print_errors(enum _ww_e_errors err_code);
+int _ww_print_errors(enum _ww_e_errors err_code);
 
-int		_ww_parse_argv(char *argv[]);
+int _ww_parse_argv(char *argv[]);
 
-int		_ww_map_file_into_memory(const char *filename);
-void	_ww_process_mapped_data();
-int		_ww_write_processed_data_to_file(void);
+int _ww_map_file_into_memory(const char *filename);
+void _ww_process_mapped_data();
+int _ww_write_processed_data_to_file(void);
 
-void	xor_encrypt_decrypt(void *key, size_t key_length, void *data, size_t data_length);
-char*	_ww_keygen(const char* _charset, size_t _strength);
+void xor_encrypt_decrypt(void *key, size_t key_length, void *data, size_t data_length);
+char *_ww_keygen(const char *_charset, size_t _strength);
 
 #endif
