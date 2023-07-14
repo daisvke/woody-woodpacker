4308ea32b76f92e470453a3fd91db96bdac3ae14
dtanigaw <dtanigaw@student.42.fr>
2023-07-14 22:56:53 +0200

    Correct Makefile + Add interp in exception

--- srcs/parse_argv.c
+++ srcs/parse_argv.c
@@ -5,9 +5,9 @@ int _ww_parse_argv(char *argv[])
 	// ----------------------For testing----------------------------
 	// Region from the source file to encrypt
 	_modes |= _WW_CYPTREG_PHDR,			// Select region using segments
-	// _modes |= _WW_CYPTREG_PHTEXTX;		// .text segments with x rights
+	_modes |= _WW_CYPTREG_PHTEXTX;		// .text segments with x rights
 	// _modes |= _WW_CYPTREG_PHTEXT,		// .text segments
-	_modes |= _WW_CYPTREG_PHALL,			// All segments
+	// _modes |= _WW_CYPTREG_PHALL,			// All segments
 	// _modes |= _WW_CYPTREG_SHDR, // Select region using sections
 	// _modes |= _WW_CYPTREG_SHTEXT,	 	// text section
 	// _modes |= _WW_CYPTREG_SHDATA,	 	// data section
--- srcs/process_data.c
+++ srcs/process_data.c
@@ -33,7 +33,7 @@ void elf_print_sections_name(Elf64_Ehdr *data)
 static int	_ww_authorize_encryption(int _type, int _flags)
 {
 		// Desactivate program header encryption
-	if ((_type != PT_PHDR) &&
+	if ((_type != PT_PHDR && _type != PT_INTERP) &&
 		// Desactivate encryption of non executable .text segment (causes segfault)
 		!(_type == PT_LOAD && !(_flags & PF_X)) &&
 		(
@@ -70,11 +70,11 @@ static void _ww_encrypt_segments(Elf64_Ehdr *_elf_header, char *_key)
 				// the latter represents the size of the segment in memory,
 				// which may include additional padding or allocated memory that
 				// we do not need to encrypt here.
-				// xor_encrypt_decrypt(
-				// 	_key, _WW_KEYSTRENGTH,
-				// 	_mapped_data + _program_header->p_offset,
-				// 	_program_header->p_filesz
-				// );
+				xor_encrypt_decrypt(
+					_key, _WW_KEYSTRENGTH,
+					_mapped_data + _program_header->p_offset,
+					_program_header->p_filesz
+				);
 			}
 			// If segment padding injectin mode is on.
 			if (_modes & _WW_INJTREG_PAD)
