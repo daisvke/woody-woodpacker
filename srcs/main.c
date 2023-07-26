2af4b06eb49e1a168004fac6925731a956a90855
Stone <stone.dummett@gmail.com>
2023-07-26 16:48:04 +0200

    Display the selected modes and setting a default modes if not selected

--- srcs/main.c
+++ srcs/main.c
@@ -8,43 +8,44 @@ void _ww_help(char *progname, int fd) {
 	_ww_dprintf(fd, "Usage: %s: [option(s)] [file(s)]\n", progname);
 	_ww_dprintf(fd, "Pack [files(s)] by encrypting sections or segments (a.out by default).\n");
 	_ww_dprintf(fd, " The options are: \n");
-	_ww_dprintf(fd, "  -r\e[4mlevel\e[0m,	--region=\e[4mlevel\e[0m		Select the region to encrypt\n");
+	_ww_dprintf(fd, "  -r\e[4mlevel\e[0m,				Select the region to encrypt\n");
 	// TODO: Explain for each level what will be encrypted here
-	// TODO: Add an underline to `level`
 	_ww_dprintf(fd, "  -p,		--padding		Select the padding injection method\n");
 	_ww_dprintf(fd, "  -s,		--shifting		Select the shifting injection method\n");
 	// TODO: Maybe be more clear about the code injection methods (?)
 	_ww_dprintf(fd, "  -h,		--help			Display this information\n");
 }
 
-void what_options() {
-	printf(YEL"What options are set: \n");
+void _ww_display_modes() {
 	if (_ww_is_option_set(&_modes, _WW_CYPTREG_PHDR))
-		printf("_WW_CYPTREG_PHDR\n");
-	if (_ww_is_option_set(&_modes, _WW_CYPTREG_PHTEXTX))
-		printf("_WW_CYPTREG_PHTEXTX\n");
-	if (_ww_is_option_set(&_modes, _WW_CYPTREG_PHTEXT))
-		printf("_WW_CYPTREG_PHTEXT\n");
-	if (_ww_is_option_set(&_modes, _WW_CYPTREG_PHALL))
-		printf("_WW_CYPTREG_PHALL\n");
-	if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHDR))
-		printf("_WW_CYPTREG_SHDR\n");
-	if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHTEXT))
-		printf("_WW_CYPTREG_SHTEXT\n");
-	if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHDATA))
-		printf("_WW_CYPTREG_SHDATA\n");
-	if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHALL))
-		printf("_WW_CYPTREG_SHALL\n");
-	if (_ww_is_option_set(&_modes, _WW_INJTREG_PAD))
-		printf("_WW_INJTREG_PAD\n");
-	if (_ww_is_option_set(&_modes, _WW_INJTREG_SFT))
-		printf("_WW_INJTREG_SFT\n");
-	if (_ww_is_option_set(&_modes, _WW_INJTREG_END))
-		printf("_WW_INJTREG_END\n");
-	if (_ww_is_option_set(&_modes, _WW_HELP))
-		printf("_WW_HELP\n");
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: _WW_CYPTREG_PHDR (this is maybe not needed).\n");
+	else if (_ww_is_option_set(&_modes, _WW_CYPTREG_PHTEXTX))
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: .text segments with x rights.\n");
+	else if (_ww_is_option_set(&_modes, _WW_CYPTREG_PHTEXT))
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: .text segments.\n");
+	else if (_ww_is_option_set(&_modes, _WW_CYPTREG_PHALL))
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: all the segments.\n");
+	else if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHDR))
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: _WW_CYPTREG_SHDR (this is maybe not needed).\n");
+	else if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHTEXT))
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: text section.\n");
+	else if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHDATA))
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: data section.\n");
+	else if (_ww_is_option_set(&_modes, _WW_CYPTREG_SHALL))
+		_ww_dprintf(STDOUT_FILENO, "Region to encrypt: text + data sections.\n");
+	else { // Setting a default region to encrypt
+		_modes |= _WW_CYPTREG_PHALL;
+		dprintf(STDOUT_FILENO, "No region to encrypt selected, default to all the segments.\n");
+	}
 
-	printf("\n"RESET);
+	if (_ww_is_option_set(&_modes, _WW_INJTREG_PAD))
+		_ww_dprintf(STDOUT_FILENO, "Code injection method: insert inside segments paddings.\n");
+	else if (_ww_is_option_set(&_modes, _WW_INJTREG_SFT))
+		_ww_dprintf(STDOUT_FILENO, "Code injection method: insert at 0x0 and shift the rest.\n");
+	else { // Setting a default code injection method
+		_modes |= _WW_INJTREG_PAD;
+		_ww_dprintf(STDOUT_FILENO, "No code injection method selected, default to inserting inside segments paddings.\n");
+	}
 }
 
 int main(int argc, char *argv[])
@@ -61,8 +62,7 @@ int main(int argc, char *argv[])
 		_ww_help(argv[0], STDOUT_FILENO);
 		return 0;
 	}
-
-	what_options();
+	_ww_display_modes();
 	return 42;
 
 	if (_ww_map_file_into_memory(argv[1]) == _WW_ERROR)
