01ce37ea3a34198b281b42e68e55e625d87b2ad1
Stone <stone.dummett@gmail.com>
2023-08-01 13:51:17 +0200

    Search through argv the binary name, if more than 1 return an error

--- srcs/main.c
+++ srcs/main.c
@@ -5,15 +5,22 @@ off_t       _file_size;
 uint16_t    _modes;
 
 void _ww_help(char *progname, int fd) {
-	_ww_dprintf(fd, "Usage: %s: [option(s)] [file(s)]\n", progname);
-	_ww_dprintf(fd, "Pack [files(s)] by encrypting sections or segments (a.out by default).\n");
+	_ww_dprintf(fd, "Usage: %s [option(s)] <binary>\n", progname);
+	_ww_dprintf(fd, "Pack <file> by encrypting sections or segments.\n");
 	_ww_dprintf(fd, " The options are: \n");
-	_ww_dprintf(fd, "  -r\e[4mlevel\e[0m,				Select the region to encrypt\n");
-	// TODO: Explain for each level what will be encrypted here
-	_ww_dprintf(fd, "  -p,		--padding		Select the padding injection method\n");
-	_ww_dprintf(fd, "  -s,		--shifting		Select the shifting injection method\n");
-	// TODO: Maybe be more clear about the code injection methods (?)
-	_ww_dprintf(fd, "  -h,		--help			Display this information\n");
+	_ww_dprintf(fd, "  -r\e[4mlevel\e[0m		Select the region to encrypt.\n");
+	_ww_dprintf(fd, "   The levels:\n");
+	_ww_dprintf(fd, "    \e[4m0\e[0m: _WW_CYPTREG_PHDR (this is maybe not needed).\n");
+	_ww_dprintf(fd, "    \e[4m1\e[0m: text segments with x rights.\n");
+	_ww_dprintf(fd, "    \e[4m2\e[0m: .text segments.\n");
+	_ww_dprintf(fd, "    \e[4m3\e[0m: all the segments.\n");
+	_ww_dprintf(fd, "    \e[4m4\e[0m: _WW_CYPTREG_SHDR (this is maybe not needed).\n");
+	_ww_dprintf(fd, "    \e[4m5\e[0m: text section.\n");
+	_ww_dprintf(fd, "    \e[4m6\e[0m: data section.\n");
+	_ww_dprintf(fd, "    \e[4m7\e[0m: text + data sections.\n");
+	_ww_dprintf(fd, "  -p,		--padding		Insert the code inside the segments padding.\n");
+	_ww_dprintf(fd, "  -s,		--shifting		Insert the code at 0x0 and shift the rest.\n");
+	_ww_dprintf(fd, "  -h,		--help			Display this information.\n");
 }
 
 void _ww_display_modes() {
@@ -48,11 +55,24 @@ void _ww_display_modes() {
 	}
 }
 
+char *_ww_get_bin_name(char **argv, int argc) {
+    char * bin_name = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] != '\0' && bin_name == NULL)
+            bin_name = argv[i];
+        else if (argv[i][0] != '\0') {
+            _ww_dprintf(STDOUT_FILENO, RED"You must provide only one binary.\n"RESET);
+            return NULL;
+        }
+    }
+    return bin_name;
+}
+
 int main(int argc, char *argv[])
 {
-	// if (argc != 2)
-	// 	return _ww_print_errors(_WW_ERR_BADARGNBR);
-
+	// After being processed by _ww_get_opt, all the options in
+	// the argv array are empty string
 	if (_ww_get_opt(argv, argc, &_modes) == _WW_ERROR) {
 		_ww_help(argv[0], STDOUT_FILENO);
 		return 1;
@@ -63,9 +83,12 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 	_ww_display_modes();
-	return 42;
 
-	if (_ww_map_file_into_memory(argv[1]) == _WW_ERROR)
+    char *binary_name = _ww_get_bin_name(argv, argc);
+    if (binary_name == NULL)
+        return 1;
+
+	if (_ww_map_file_into_memory(binary_name) == _WW_ERROR)
 		return 1;
 
 	_ww_process_mapped_data();
